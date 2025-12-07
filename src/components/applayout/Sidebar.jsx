1c4227c56cbd736e1251ad8964ad17cb8fc72156
sayeed <sayeed034278@gmail.com>
2025-12-07 12:58:24 +0530

    updated sidebar heading

--- src/components/applayout/Sidebar.jsx
+++ src/components/applayout/Sidebar.jsx
@@ -11,11 +11,27 @@ export default function Sidebar({ activeCategoryId, setActiveCategoryId }) {
 
   return (
     <div className="  p-2 h-full flex flex-col min-h-0 rounded-2xl rounded-tr-none rounded-br-none bg-white/7 backdrop-blur-xl shadow-[0_0_50px_rgba(0,0,0,0.3)] verflow-hidden
-        border border-white/10 border-r-0        
-      "           
+        border border-white/10 border-r-0    
+      "
     >
 
-      <h1 className="text-3xl font-semibold mb-3">Just Do it</h1>
+      {/* logo 
+      <div className="border  p-0.5 rounded-sm w-fit">
+        <h1 className="text-white text-3xl font-semibold ">Bare</h1>
+        <span className="text-2xl font-bold bg-white text-black inline-block">Minimum</span>
+        <p className="text-white/40 text-2xl font-medium">app</p>
+      </div>
+      */}
+      <div className=" w-fit">
+        <h1 className="text-white text-3xl font-semibold ">Barely</h1>
+        <div className="inline-flex items-baseline">
+          <span className="text-2xl font-bold bg-white px-1 text-black inline-block">Do</span>
+          <p className="text-white/40 text-2xl font-medium  ml-1">It.</p>
+        </div>
+      </div>
+
+
+      {/* <img src="src\assets\favicon.png" alt="favicon" /> */}
 
       {/* ▼ Smooth collapsible add-category container */}
       <div className={`transition-all duration-300 overflow-hidden pr-2`}>
